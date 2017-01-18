808f421e3669f5e9ef625eb4ac7021af921b0e05
Jake Waksbaum <jake.waksbaum@gmail.com>
2017-01-18 18:42:19 -0500

    nodePackages: Add js-beautify

--- pkgs/development/node-packages/node-env.nix
+++ pkgs/development/node-packages/node-env.nix
@@ -1,10 +1,8 @@
 # This file originates from node2nix
 
-{stdenv, nodejs, utillinux, runCommand, writeTextFile}:
+{stdenv, python, nodejs, utillinux, runCommand, writeTextFile}:
 
 let
-  inherit (nodejs) python;
-
   # Create a tar wrapper that filters all the 'Ignoring unknown extended header keyword' noise
   tarWrapper = runCommand "tarWrapper" {} ''
     mkdir -p $out/bin
