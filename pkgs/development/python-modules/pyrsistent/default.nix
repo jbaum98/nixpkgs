44e092f84a5b4dc3d36bda7b23c6387df60e318c
worldofpeace <worldofpeace@users.noreply.github.com>
2019-01-09 16:17:45 -0500

    fixup! pythonPackages.pyristent: add pytestrunner, fix build

--- pkgs/development/python-modules/pyrsistent/default.nix
+++ pkgs/development/python-modules/pyrsistent/default.nix
@@ -16,14 +16,12 @@ buildPythonPackage rec {
     sha256 = "5a31f6b093da3401fefdeb53a0980e3145bb9d2bf852b579cc7b39c7f0016c87";
   };
 
-  nativeBuildInputs = [ pytestrunner ];
-
   propagatedBuildInputs = [ six ];
 
-  checkInputs = [ pytest hypothesis ];
-  checkPhase = ''
-    pytest
-  '';
+  checkInputs = [ pytestrunner pytest hypothesis ];
+
+  # pytestrunner is only needed to run tests
+  patches = [ ./no-setup-requires-pytestrunner.patch ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/tobgu/pyrsistent/;
